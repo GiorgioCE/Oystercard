d57b078f13158664c999ee9e801a39ebb78e4c4e
Lorna Roberts <lornaroberts99@gmail.com>
2020-10-20 15:05:38 +0100

    journey status

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,7 +2,7 @@ class Oystercard
 
 MAXIMUM_BALANCE = 90
 
-attr_reader :balance
+attr_reader :balance, :state
   def initialize
     @balance = 0
 
@@ -14,6 +14,22 @@ fail ("Can't exceed 90") if amount + balance > MAXIMUM_BALANCE
 
   @balance += amount
 
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @state
+  end
+
+  def touch_in
+  @status = "start"
+  end
+
+  def touch_out
+    @status = "end"
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,4 +23,27 @@ RSpec.describe Oystercard do
       expect{ subject.top_up 1 }.to raise_error "Can't exceed #{maximum_balance}"
     end
   end
+   describe "#deduct" do
+     it "deducts money from the card" do
+       subject.top_up 20
+       expect{subject.deduct 1}.to change { subject.balance}.by -1
+     end
+   end
+   describe "#in_journey?" do
+     it "shows if we are in the middle of a journey" do
+       @state = true
+       expect(subject.in_journey?).to eq(nil)
+     end
+   end
+   describe "#touch_in" do
+     it "tracks when you start a journey" do
+       expect(subject.touch_in).to eq("start")
+     end
+   end
+   describe "#touch_out" do
+     it "tracks when you end a journey" do
+       expect(subject.touch_out).to eq("end")
+     end
+   end
+
 end
